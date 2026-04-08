0d3eb8cda564273f203262add4492e03403a9e7b
anirbandotdev <anirbaniitian@gmail.com>
2026-04-08 16:50:15 +0530

    tweak: add database(mongo) logic and update env

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,5 +6,7 @@ config({
 
 const PORT = process.env.PORT;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const DB_URI = process.env.DB_URI;
+const DB_NAME = process.env.DB_NAME;
 
-export { PORT, CORS_ORIGIN };
+export { PORT, CORS_ORIGIN, DB_URI, DB_NAME };
